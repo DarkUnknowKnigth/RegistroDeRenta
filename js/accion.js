cc57509d06fb2b65322af56720d4289f7a0f276f
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2017-12-08 00:13:25 -0600

    Agregue la funcionalidad de las tabs

--- js/accion.js
+++ js/accion.js
@@ -1,7 +1,36 @@
 //carga los componentes y valores
+//variables
+var ingreso=$("#ingreso");
+var consulta=$("#consulta");
 function set_load()
 {
-    $("#datepicker").datepicker();
+    $("#datepicker").datepicker({
+        dateFormat: "yy-mm-dd"
+      });
+    $("#ingreso").click(function(event){
+        change(event,"reg");
+        
+    });
+    $("#consulta").click(function(event){
+        change(event,"resp");
+    });
+}
+function change(event,id){
+    var tabcontent, tablinks;
+    tabcontent = $(".tabcontent");
+    //console.log(tabcontent);
+    for (i = 0; i < tabcontent.length; i++) 
+    {
+       tabcontent[i].style.display = "none";
+    }
+    tablinks = $(".tablinks");
+    //console.log(tablinks);
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active","");
+    }
+    $("#"+id).css("display","block");
+    event.currentTarget.className += " active";
+
 }
 $(document).ready(function(){
     if(jQuery)
