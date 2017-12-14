b07e464b5001ac7d6cf9b5ac16f3fa6989c96f3a
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2017-12-13 21:09:34 -0600

    agregado a js validacion, index con ids y nuevas class,agregue ajax para que no se recargue

--- js/accion.js
+++ js/accion.js
@@ -2,36 +2,117 @@
 //variables
 var ingreso=$("#ingreso");
 var consulta=$("#consulta");
+var t_numero=false,t_nom=false;
+var error=$("#error");
+/*-----configuracion de los componentes iniciales----*/
 function set_load()
 {
-    $("#datepicker").datepicker({
-        dateFormat: "yy-mm-dd"
-      });
-    $("#ingreso").click(function(event){
-        change(event,"reg");
-        
+    t_nom=false;
+    t_numero=false;
+    $("#datepicker").datepicker({dateFormat: "yy-mm-dd"});
+    $("#ingreso").click(function(event){change(event,"reg");});
+    $("#consulta").click(function(event){change(event,"resp");});
+    $("#submit").click(function(e)
+    {
+        (valida_f())?(console.log("se valido")):(e.preventDefault());
     });
-    $("#consulta").click(function(event){
-        change(event,"resp");
-    });
+    $("#reset").click(function(e){});
+    $("#monto").click(function(e){test_numero(e);}).prop("min",0).keypress(function(e){test_numero(e)});   
 }
+/*--------------funcion de tabs------------------------*/
 function change(event,id){
     var tabcontent, tablinks;
     tabcontent = $(".tabcontent");
-    //console.log(tabcontent);
     for (i = 0; i < tabcontent.length; i++) 
     {
        tabcontent[i].style.display = "none";
     }
     tablinks = $(".tablinks");
-    //console.log(tablinks);
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active","");
     }
     $("#"+id).css("display","block");
     event.currentTarget.className += " active";
-
 }
+/*--------------validacion de  formularios---------------*/
+function test_numero(evento)
+{  
+    {
+        console.log(t_numero);
+        //verificar campos no permitidos
+        if(evento.keyCode==69 || evento.keyCode==101 || evento.keyCode==45 || evento.keyCode==43) 
+        {
+            evento.preventDefault();
+            error.text("Solo numeros");     
+            t_numero=false;      
+        }  
+        else if($("#"+evento.target.id).val()>0)
+        {
+            t_numero=true;
+        } 
+        else if($("#"+evento.target.id).val()<0)
+        {
+           $("#"+evento.target.id).val("");
+            error.text("No se permiten numeros negativos");
+           t_numero=false;    
+        }
+        else
+        {
+            error.text("Solo numeros");
+        }
+    }
+}//probar texto de todos los campos 
+function test_nom(nombre)
+{
+    for(var i=0;i<nombre.length;i++)
+    {
+        if(/^[A-z\.\-\s]+$/.test($("#"+nombre[i].id).val()))
+        {
+            t_nom=true;
+        }
+        else
+        {
+            t_nom=false;
+            error.text("Ha ingresado caracteres invalidos");
+
+        }
+        console.log(t_nom);    
+    }
+}
+//validacion de todo el formulario
+function valida_f()
+{
+    test_nom($(".texto"));
+    if(t_nom && t_numero && $("#datepicker").val().length>0)
+    {
+        var parametros={
+            "pnombre":$("#np").val(),
+            "papellido":$("ap").val(),
+            "inombre":$("#in").val(),
+            "iapellido":$("#ai").val(),
+            "monto":$("#monto").val(),
+            "fecha":$("#fecha").val()
+        }
+        var send=$.post("php/index.php",
+        parametros).done(function( data )
+        {
+            $("#error").css("color","#009933").html("Registro enviado correctamente");
+        }).fail(function(data){
+            $("#error").css("color","#ff0000").html("No se pudo enviar el registro");
+
+        });
+        console.log(send);
+        set_load();
+        return true;
+    }
+    else
+    {    
+        error.text("Campos invalidos o vacios");
+        t_nom=false;
+        return false;
+    }
+}
+/*-----------carga de la funcionalidad------------*/
 $(document).ready(function(){
     if(jQuery)
     {
