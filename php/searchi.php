a5381a482d3235bfe5e650574db59ad8ce91e11a
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2017-12-31 01:12:44 -0600

    nuevas respuestas del servidor para las busquedas de inquilino y propietario

---
+++ php/searchi.php
@@ -0,0 +1,128 @@
+<?php
+    $url="localhost";
+    $pass="";
+    $db="cobro";
+    $user="root";
+    $conn= new mysqli($url,$user,$pass,$db);
+    if(!preg_match('/^[a-zA-Z0-9, ]*$/',$_POST['palabra']))
+    {
+        die("busqueda invalida");
+    }
+    if($conn->connect_error)
+    {
+        die("fallo en la coneccion: "+$conn->conenct_error);
+    }
+    $palabra=$_POST['palabra'];
+    $tipo=$_POST['tipo'];
+    switch($tipo)
+    {
+        case "nombre":
+            $sql = "SELECT
+            inquilino.idinquilino as ID,
+            inquilino.nombre as NI, 
+            inquilino.apellido as AI
+            FROM inquilino 
+            WHERE inquilino.nombre LIKE '%".$palabra."%'";
+            $result = $conn->query($sql);
+            $tabla="";
+            if ($result->num_rows > 0) 
+            {
+                while($row = $result->fetch_assoc()) 
+                {
+                    $tabla.= 
+                    "<tr id=tri_".$row["ID"].">
+                    <td>".$row["ID"]."</td>
+                    <td>".$row["NI"]."</td>
+                    <td>".$row["AI"]."</td>
+                    <td>
+                        <input type= 'radio' class='btni_m_ ' name='rowi' value='".$row["ID"]."'>
+                    </td>
+                    </tr>";
+                }
+                echo $tabla;
+                break;
+            } 
+            else 
+            {
+                echo "0 results";
+                break;
+            }
+        case "apellido":
+            $sql = "SELECT 
+            idinquilino as ID,
+            nombre as NI, 
+            apellido as AI
+            FROM inquilino 
+            WHERE inquilino.apellido LIKE '%".$palabra."%';";
+            $result = $conn->query($sql);
+            $tabla="";
+            if ($result->num_rows > 0) 
+            {
+                // output data of each row
+                while($row = $result->fetch_assoc()) 
+                {
+                    $tabla.= 
+                    "<tr id=tri_".$row["ID"].">
+                    <td>".$row["ID"]."</td>
+                    <td>".$row["NI"]."</td>
+                    <td>".$row["AI"]."</td>
+                    <td>
+                        <input type= 'radio' class='btni_m_ ' name='rowi' value='".$row["ID"]."'>
+                    </td>
+                    </tr>";
+                }
+                echo $tabla;
+                break;
+            } 
+            else 
+            {
+                echo "0 results";
+                break;
+            }
+        case "id":
+            $sql = "SELECT 
+            inquilino.idinquilino as ID,
+            inquilino.nombre as NI, 
+            inquilino.apellido as AI
+            FROM inquilino 
+            WHERE inquilino.idinquilino LIKE ".$palabra;
+            $palabra=(int)$palabra;
+            if(is_int($palabra) && $palabra!=null)
+            {
+                $result = $conn->query($sql);
+                $tabla="";
+                if ($result->num_rows > 0) 
+                {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) 
+                    {
+                        $tabla.= 
+                        "<tr id=tri_".$row["ID"].">
+                        <td>".$row["ID"]."</td>
+                        <td>".$row["NI"]."</td>
+                        <td>".$row["AI"]."</td>
+                        <td>
+                            <input type= 'radio' class='btni_m_ ' name='rowi' value='".$row["ID"]."'>
+                        </td>
+                        </tr>";
+                    }
+                    echo $tabla;
+                    break;
+                } 
+                else 
+                {
+                    echo "0 results";
+                    break;
+                }  
+            }
+            else
+            {
+                die("caracteres no validos");
+            }
+        default:
+            echo 'no se encontro un tipo de dato valido';
+        break;
+    }
+    $conn->close();
+
+?>
---
+++ php/searchp.php
@@ -0,0 +1,114 @@
+<?php
+    $url="localhost";
+    $pass="";
+    $db="cobro";
+    $user="root";
+    $conn= new mysqli($url,$user,$pass,$db);
+    if(!preg_match('/^[a-zA-Z0-9, ]*$/',$_POST['palabra']))
+    {
+        die("busqueda invalida");
+    }
+    if($conn->connect_error)
+    {
+        die("fallo en la coneccion: "+$conn->conenct_error);
+    }
+    $palabra=$_POST['palabra'];
+    $tipo=$_POST['tipo'];
+    switch($tipo)
+    {
+        case "nombre":
+            $sql = "SELECT idpropietario,nombre,apellido FROM propietario WHERE nombre LIKE '%".$palabra."%'";
+            $result = $conn->query($sql);
+            $tabla="";
+            if ($result->num_rows > 0) 
+            {
+                // output data of each row
+                while($row = $result->fetch_assoc()) 
+                {
+                    $tabla.= 
+                    "<tr id=trp_".$row["idpropietario"].">
+                    <td>".$row["idpropietario"]."</td>
+                    <td>".$row["nombre"]."</td>
+                    <td>".$row["apellido"]."</td>
+                    <td class="."col-auto mr-auto".">
+                        <input type= 'radio' class='btnp_m_' name='rowp' value='".$row["idpropietario"]."'>
+                    </td>
+                    </tr>";
+                }
+                echo $tabla;
+                break;
+            } 
+            else 
+            {
+                echo "0 results";
+                break;
+            }
+        case "apellido":
+            $sql = "SELECT idpropietario,nombre,apellido FROM propietario WHERE apellido LIKE '%".$palabra."%'";
+            $result = $conn->query($sql);
+            $tabla="";
+            if ($result->num_rows > 0) 
+            {
+                // output data of each row
+                while($row = $result->fetch_assoc()) 
+                {
+                    $tabla.= 
+                    "<tr id=trp_".$row["idpropietario"].">
+                    <td>".$row["idpropietario"]."</td>
+                    <td>".$row["nombre"]."</td>
+                    <td>".$row["apellido"]."</td>
+                    <td class="."col-auto mr-auto".">
+                        <input type= 'radio' class='btnp_m_' name='rowp' value='".$row["idpropietario"]."'>
+                    </td>
+                    </tr>";
+                }
+                echo $tabla;
+                break;
+            } 
+            else 
+            {
+                echo "0 results";
+                break;
+            }
+        case "id":
+            $sql = "SELECT idpropietario,nombre,apellido FROM propietario WHERE idpropietario LIKE ".$palabra;
+            $palabra=(int)$palabra;
+            if(is_int($palabra) && $palabra!=null)
+            {
+                $result = $conn->query($sql);
+                $tabla="";
+                if ($result->num_rows > 0) 
+                {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) 
+                    {
+                        $tabla.= 
+                        "<tr id=tri_".$row["idpropietario"].">
+                        <td>".$row["idpropietario"]."</td>
+                        <td>".$row["nombre"]."</td>
+                        <td>".$row["apellido"]."</td>
+                        <td>
+                            <input type= 'radio' class='btni_m_ ' name='rowi' value='".$row["idpropietario"]."'>
+                        </td>
+                        </tr>";
+                    }
+                    echo $tabla;
+                    break;
+                } 
+                else 
+                {
+                    echo "0 results";
+                    break;
+                }  
+            }
+            else
+            {
+                die("caracteres no validos");
+            }
+        default:
+            echo 'no se encontro un tipo de dato valido';
+        break;
+    }
+    $conn->close();
+
+?>
